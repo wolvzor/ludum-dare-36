b0fb9da47ac3408c6aa002e2acaf2845434ebbc1
wolvie <wolvie@wolviegames.com>
2016-08-26 21:33:10 -0600

    A tiger is now being drawn.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ludum-dare-36
 Ludum Dare 36 jam time! Theme: Ancient Technology
+
+A story about a tiger, some stats, and life getting overly complicated. 
+
+Want to see the current progress?
+
+- Install SBT
+- Should probably have scala somewhere
+- Run `sbt run`
+- Look at the cute large tiger.
--- src/main/scala/com/wolviegames/tigerstory/TigerStory.scala
+++ src/main/scala/com/wolviegames/tigerstory/TigerStory.scala
@@ -1,10 +1,29 @@
 package com.wolviegames.tigerstory
 
-import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.{GL20, Texture}
+import com.badlogic.gdx.{Game, Gdx}
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class TigerStory extends Game{
 
   override def create(): Unit = {
     println("Tiger's Story")
+  }
+
+  override def render(): Unit = {
+
+    // THESE DO NOT BELONG HERE
+    // YOUR INITIALIZATION IS BAD AND YOU SHOULD FEEL BAD
+    val spriteBatch: SpriteBatch = new SpriteBatch()
+    val tigerTexture: Texture = new Texture(Gdx.files.internal("tiger.jpg"))
+
+
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Apparently this clears the screen; wtf gdx
+    spriteBatch.begin
+
+    // And then drawing things happen here O_O
+    spriteBatch.draw(tigerTexture,10,10)
+
+    spriteBatch.end
   }
 }

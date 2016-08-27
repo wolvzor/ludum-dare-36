fdda854783d4089d4d33863f112a344834586fb8
wolvie <wolvie@wolviegames.com>
2016-08-26 21:53:35 -0600

    More things are now drawn :D

--- src/main/scala/com/wolviegames/tigerstory/TigerStory.scala
+++ src/main/scala/com/wolviegames/tigerstory/TigerStory.scala
@@ -15,13 +15,36 @@ class TigerStory extends Game{
     // THESE DO NOT BELONG HERE
     // YOUR INITIALIZATION IS BAD AND YOU SHOULD FEEL BAD
     val spriteBatch: SpriteBatch = new SpriteBatch()
-    val tigerTexture: Texture = new Texture(Gdx.files.internal("tiger.jpg"))
+
+    // Different textures for funsies
+
+    // 60x60
+    val tigerTexture: Texture = new Texture(Gdx.files.internal("tiger-small.jpg"))
+
+    // 120x120
+    val boarTexture: Texture = new Texture(Gdx.files.internal("boar.jpg"))
+    val caveTexture: Texture = new Texture(Gdx.files.internal("cave.jpg"))
+    val deerTexture: Texture = new Texture(Gdx.files.internal("deer.jpg"))
+    val grassTexture: Texture = new Texture(Gdx.files.internal("grass.jpg"))
+    val mateTexture: Texture = new Texture(Gdx.files.internal("mate.jpg"))
+    val rabbitTexture: Texture = new Texture(Gdx.files.internal("rabbit.jpg"))
+
+
 
 
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Apparently this clears the screen; wtf gdx
     spriteBatch.begin
 
     // And then drawing things happen here O_O
+
+    spriteBatch.draw(caveTexture, 10, 10) // home base!
+    spriteBatch.draw(boarTexture, 120 + 10, 120 + 10)
+    spriteBatch.draw(deerTexture, 120 + 10, 10)
+    spriteBatch.draw(grassTexture, 10, 120 + 10)
+    spriteBatch.draw(mateTexture, 120 + 120 + 10, 120 + 10)
+    spriteBatch.draw(rabbitTexture, 120 + 10, 120 + 120 + 10)
+
+    // Tiger needs to go last (poor thing)
     spriteBatch.draw(tigerTexture,10,10)
 
     spriteBatch.end
